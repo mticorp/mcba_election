d342eaaca66b4a6c27f7f97dfaa0b2ab38e5bb7a
HeinHtet <heinhtetsanmti@gmail.com>
2022-01-29 05:05:09 +0000

    new

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -110,7 +110,7 @@
              </div> 
              <!-- ./col -->
               <div class="col-lg-3 col-6">
-               <div class="small-box bg-info">
+               <div class="small-box bg-success">
                  <div class="inner">
                     <h3>{{$voting_count}} </h3>
  

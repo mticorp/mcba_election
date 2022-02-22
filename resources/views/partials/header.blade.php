05a59a71edbeefcab9b1382e94027045c070f206
MTICORP <heinhtetsanmti@gmail.com>
2022-02-22 08:04:55 +0000

    new

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,13 +2,12 @@
 <nav class="navbar">
     <div class="text-white text-center w-100">
         @if((request()->is('/') ? 'active' : '') || ($request->segment(2) == 'register' ? 'active' : '') || ($request->segment(1) == 'select-Election' ? 'active' : '') || ($request->segment(1) == 'app' ? 'active' : '') || ($request->segment(1) == 'verify' ? 'active' : ''))
-            <img src="{{url('images/election_logo.png')}}" class="col-10" style="width: 300px; height: 80px; object-fit: cover" alt="" >
-        
-            @else
+            <img src="{{url($setting->logo_image ? $setting->logo_image : 'images/election_logo.png')}}" class="col-10" style="width: 300px; height: 80px; object-fit: contain" alt="" >        
+        @else
             @if($election->company_logo)
                 <img src="{{url($election->company_logo)}}" alt="" style="width: 300px; height: 80px; object-fit: cover" class=" col-12">
             @else
-            <img src="{{url('images/election_logo.png')}}" class="col-10" style="width: 300px; height: 80px; object-fit: cover" alt="">
+            <img src="{{url($setting->logo_image ? $setting->logo_image : 'images/election_logo.png')}}" class="col-10" style="width: 300px; height: 80px; object-fit: contain" alt="">
         
             @endif
         @endif
